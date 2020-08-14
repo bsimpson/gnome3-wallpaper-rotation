c293294f1f4b275b643c641fc8ad5a86c455119a
Ben Simpson <thehoagie@gmail.com>
2020-08-14 09:02:41 -0400

    Remove axios dependency

--- index.js
+++ index.js
@@ -1,30 +1,59 @@
 'use strict';
 
-const axios = require('axios').default;
 const { spawnSync } = require( 'child_process' );
 const fs = require('fs');
 const { URL } = require('url');
+const https = require('https');
 
-axios.get('https://www.reddit.com/r/WidescreenWallpaper.json')
-.then(response => {
-  return response.data.data.children.map((child) => {
-    return child.data.url_overridden_by_dest;
-  })[0];
-})
-.then(imageUrl => {
-  return axios({
-    method: 'get',
-    url: imageUrl,
-    responseType: 'stream',
-  });
-})
-.then(imageData => {
-  const filename = new URL(imageData.config.url).pathname.split('/').slice(-1)[0];
-  const wallpaperPath = `/home/${process.env.USER}/Pictures/Wallpapers/${filename}`;
-  imageData.data.pipe(fs.createWriteStream(wallpaperPath));
-  return wallpaperPath;
-})
-.then((wallpaperPath) => {
-  spawnSync( 'gsettings', [ 'set', 'org.gnome.desktop.background', 'picture-uri', `file:///${wallpaperPath}` ] );
-})
+// Feel free to change these configuration options
+const SUBREDDIT_URL = 'https://www.reddit.com/r/WidescreenWallpaper.json';
+const DOWNLOAD_PATH = `/home/${process.env.USER}/Pictures/Wallpapers`;
+
+console.log('Starting background changer');
+
+getImageList()
+.then(saveImage)
+.then(setWallpaper)
 .catch(console.error);
+
+
+function getImageList() {
+  return new Promise((resolve, reject) => {
+    https.get(SUBREDDIT_URL, (res) => {
+      let str = '';
+  
+      res.on('data', chunk => str += chunk);
+      res.on('end', () => {
+        console.log('Got list of images');
+        str = JSON.parse(str);
+        resolve(str.data.children.map((child) => {
+          return child.data.url_overridden_by_dest;
+        })[0]);
+      });
+    });
+  })
+}
+
+function saveImage(imageUrl) {
+  console.log(`Got top image URL ${imageUrl}`);
+  return new Promise((resolve, reject) => {
+    https.get(imageUrl, (res) => {
+      const filename = imageUrl.split('/').slice(-1)[0];
+      const wallpaperPath = `${DOWNLOAD_PATH}/${filename}`;
+      const fileStream = fs.createWriteStream(wallpaperPath);
+      res.pipe(fileStream);
+
+      fileStream.on('finish', () => {
+        fileStream.close(resolve(wallpaperPath));
+      });
+    });
+  });
+}
+
+function setWallpaper(pathTowallpaper) {
+  return new Promise((resolve, reject) => {
+    console.log(`Setting image ${pathTowallpaper}`);
+    spawnSync( 'gsettings', [ 'set', 'org.gnome.desktop.background', 'picture-uri', `file:///${pathTowallpaper}` ] );
+    resolve();
+  });
+}
